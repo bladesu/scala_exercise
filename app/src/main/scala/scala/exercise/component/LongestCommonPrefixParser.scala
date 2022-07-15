29e00d161d737c92bc160f05bbeb0c8099def24c
BladeSu <bladesu@inquartik.com>
2022-07-15 23:01:48 +0800

    add first version of LongestCommonPrefixParser, add test case

--- app/src/main/scala/scala/exercise/component/LongestCommonPrefixParser.scala
+++ app/src/main/scala/scala/exercise/component/LongestCommonPrefixParser.scala
@@ -1,5 +1,27 @@
 package scala.exercise.component
 
 class LongestCommonPrefixParser {
+  
+  def parse(list: List[String]): String = {
+    var commonPrefix = list(0)
+    val remainingElements = list.drop(1)
+    val totalCnt = remainingElements.size
+    var matchedCnt = 0
+    // worst case:
+    while (matchedCnt < totalCnt && commonPrefix.length > 0) {
 
+      matchedCnt = remainingElements
+        .takeWhile(e => e.startsWith(commonPrefix))
+        .size
+      
+      if (matchedCnt < totalCnt)
+        commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1)
+    }
+  
+    commonPrefix
+  }
+  
+  private def keepCommonPrefix(a: String, b: String) = {
+  
+  }
 }
---
+++ app/src/test/scala/scala/exercise/component/LongestCommonPrefixParserTest.scala
@@ -0,0 +1,36 @@
+package scala.exercise.component
+
+import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class LongestCommonPrefixParserTest extends AnyFunSuite {
+  
+  var longestCommonPrefixParser = new LongestCommonPrefixParser
+  
+  test("two different word: no common prefix") {
+    assert(longestCommonPrefixParser.parse(List("ab","cd")) == "")
+  }
+  
+  test("two same word and one different word: no common prefix") {
+    assert(longestCommonPrefixParser.parse(List("ab","ab","cd")) == "")
+  }
+  
+  test("test single word, => whole word is the common prefix") {
+    assert(longestCommonPrefixParser.parse(List("ab1")) == "ab1")
+  }
+  
+  test("test two identical word => whole word is the common prefix") {
+    assert(longestCommonPrefixParser.parse(List("abc", "abc")) == "abc")
+  }
+  
+  test("test three word with same length with prefix ab") {
+    assert(longestCommonPrefixParser.parse(List("ab1","ab2", "ab3")) == "ab")
+  }
+  
+  test("test three word with different length with prefix ab") {
+    assert(longestCommonPrefixParser.parse(List("abbb","abbbbbbbbbbbbbbb2", "ab")) == "ab")
+  }
+  
+}
